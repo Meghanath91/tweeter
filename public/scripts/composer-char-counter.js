182a0476e155141eda7c3768816145a83709cb04
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-29 06:45:06 +0000

    tweet container designed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,25 +1,13 @@
 $(document).ready(function() {
-
-  $("#tweetArea").keydown(function (e) { 
-   let counter = 140;
-   let numberOfChar = $(this).val().length;
-   let result = counter - numberOfChar;
-   $(".counter").text(result);
-   if (result < 0) {
-     $(".counter").css("color", "red");
-   } else {
-    $(".counter").css("color", "black");
-   }
+  $("#tweetArea").keydown(function(e) {
+    let counter = 140;
+    let numberOfChar = $(this).val().length;
+    let result = counter - numberOfChar;
+    $(".counter").text(result);
+    if (result < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "black");
+    }
   });
-
 });
-
-
-// let n = 140;
-// document.addEventListener("keypress", (event)=>{
-//   console.log(event);
-//   console.log(n--);
-//   //return n--
-// })
-
-
